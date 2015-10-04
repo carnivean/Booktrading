ec632bf71121e59da270b7dbaa66ee14ee315aee
Erik Kynast <erikkynast@gmail.com>
2015-10-04 20:25:14 +0200

    initial commit

---
+++ client/app/admin/admin.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('booktradingApp')
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/admin', {
+        templateUrl: 'app/admin/admin.html',
+        controller: 'AdminCtrl'
+      });
+  });
